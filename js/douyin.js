4313f73fb66d19f2e774a8a5956169b11c6869b6
vision <1334822971@qq.com>
2019-11-08 15:48:18 +0800

    optimize(video):优化加载

--- js/douyin.js
+++ js/douyin.js
@@ -9,8 +9,32 @@ window.onload = function () {
   document.addEventListener('WeixinJSBridgeReady', () => {
     console.log('WeixinJSBridgeReady')
   }, false)
+  if (parseUA().android) {
+    const elem = document.querySelector('.filter');
+    elem.style.display = 'none'
+  }
 
 //tools
+  function parseUA () {
+    const u = navigator.userAgent;
+    const u2 = navigator.userAgent.toLowerCase();
+    return { //移动终端浏览器版本信息
+      trident: u.indexOf('Trident') > -1, //IE内核
+      presto: u.indexOf('Presto') > -1, //opera内核
+      webKit: u.indexOf('AppleWebKit') > -1, //苹果、谷歌内核
+      gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1, //火狐内核
+      mobile: !!u.match(/AppleWebKit.*Mobile.*/), //是否为移动终端
+      ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端
+      android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1, //android终端或uc浏览器
+      iPhone: u.indexOf('iPhone') > -1, //是否为iPhone或者QQHD浏览器
+      iPad: u.indexOf('iPad') > -1, //是否iPad
+      webApp: u.indexOf('Safari') == -1, //是否web应该程序，没有头部与底部
+      iosv: u.substr(u.indexOf('iPhone OS') + 9, 3),
+      weixin: u2.match(/MicroMessenger/i) == 'micromessenger',
+      ali: u.indexOf('AliApp') > -1,
+    };
+  }
+
   function closeModal () {
     const confirmElem = document.getElementById('confirm')
     confirmElem.classList.remove('show')
@@ -70,6 +94,9 @@ window.onload = function () {
   }
 
   function renderCover (elem) {
+    if (parseUA().android) {
+      return
+    }
     const cover = elem.getAttribute('cover-src') || 'https://images.freeimages.com/images/small-previews/815/birds-ii-1379356.jpg'
     const filterElem = document.querySelector('.filter')
     filterElem.style.backgroundImage = `url(${cover})`
